927c278ed0140d844c994b1e52919b189e526240
Sahil Virdi <sahilvirdiji2066@gmail.com>
2026-05-22 15:26:23 +0000

    fixed code errors

--- Code/shapes.py
+++ Code/shapes.py
@@ -4,25 +4,25 @@ class ShapeCircle(Scene):
     def construct(self):
         circle = Circle(radius=2,color=RED)
         self.play(Create(circle))
-        self.wait(2)
+        self.wait(1)
 
 class ShapeSquare(Scene):
     def construct(self):
-        circle = Circle(radius=2,color=RED)
+        square = Square(side_length=2,color=RED)
         self.play(Create(square))
-        self.wait(2)
+        self.wait(1)
 
 class ShapeTriangle(Scene):
     def construct(self):
         triangle = Triangle(color=PURPLE)
         self.play(Create(triangle))
-        self.wait(2)
+        self.wait(1)
 
 class Shapes(Scene):
     def construct(self):
         circle = Circle(radius=1,color=RED)
         square = Square(color=PINK)
-        triangle = Triangle(color=PURPLE)
+        triangle = Triangle(color=PURPLE).set_width(2)
 
         square.shift(LEFT*3)
         triangle.shift(RIGHT*3)
@@ -32,6 +32,6 @@ class Shapes(Scene):
         self.play(Create(square))
         self.wait(1)
         self.play(Create(triangle))
-        self.wait(2)
+        self.wait(1)
 
         self.play(FadeOut(circle),FadeOut(square), FadeOut(triangle))
