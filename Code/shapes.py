0ad8cb2a4c4c3a7737dfc552aa2446dc9dc291c9
Sahil Virdi <sahilvirdiji2066@gmail.com>
2026-05-22 08:25:19 +0000

    changed the code

--- Code/shapes.py
+++ Code/shapes.py
@@ -1,14 +1,3 @@
-# from manim import *
-
-# class FirstScene(Scene):
-#     def construct(self):
-#         # This is where all the magic happens
-#         circle = Circle(radius=2, color=BLUE)
-#         self.play(Create(circle))
-#         self.wait(2)          # Wait 2 seconds
-        
-
-
 from manim import *
 
 class ShapeCircle(Scene):
