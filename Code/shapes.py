17f9a66980452659e62c747c4ae6b92a474f3e46
Sahil Virdi <sahilvirdiji2066@gmail.com>
2026-05-22 09:09:47 +0000

    code to create single and multiple shapes

--- Code/shapes.py
+++ Code/shapes.py
@@ -5,3 +5,33 @@ class ShapeCircle(Scene):
         circle = Circle(radius=2,color=RED)
         self.play(Create(circle))
         self.wait(2)
+
+class ShapeSquare(Scene):
+    def construct(self):
+        circle = Circle(radius=2,color=RED)
+        self.play(Create(square))
+        self.wait(2)
+
+class ShapeTriangle(Scene):
+    def construct(self):
+        triangle = Triangle(color=PURPLE)
+        self.play(Create(triangle))
+        self.wait(2)
+
+class Shapes(Scene):
+    def construct(self):
+        circle = Circle(radius=1,color=RED)
+        square = Square(color=PINK)
+        triangle = Triangle(color=PURPLE)
+
+        square.shift(LEFT*3)
+        triangle.shift(RIGHT*3)
+
+        self.play(Create(circle))
+        self.wait(1)
+        self.play(Create(square))
+        self.wait(1)
+        self.play(Create(triangle))
+        self.wait(2)
+
+        self.play(FadeOut(circle),FadeOut(square), FadeOut(triangle))
