bc281fc52437fd8b9fa5598db8a0c15fa2cca2c4
Sahil Virdi <sahilvirdiji2066@gmail.com>
2026-05-25 12:09:01 +0000

    added transform animation code

--- Code/shape_Transform.py
+++ Code/shape_Transform.py
@@ -1,23 +1,18 @@
 from manim import *
 
-class TwoTransforms(Scene):
-    def transform(self):
-        a = Circle()
-        b = Square()
-        c = Triangle()
-        self.play(Transform(a, b))
-        self.play(Transform(a, c))
-        self.play(FadeOut(a))
 
-    def replacement_transform(self):
-        a = Circle()
-        b = Square()
-        c = Triangle()
-        self.play(ReplacementTransform(a, b))
-        self.play(ReplacementTransform(b, c))
-        self.play(FadeOut(c))
-
+class TransformExample(Scene):
     def construct(self):
-        self.transform()
-        self.wait(0.5)  # wait for 0.5 seconds
-        self.replacement_transform()
+        title = Text("Transform Animation").to_edge(UP)
+
+        circle = Circle(color=BLUE,fill_opacity=0.5)
+        square = Square(color=GREEN,fill_opacity=0.5)
+
+        self.play(Write(title))
+        self.play(Create(circle))
+        self.wait(1)
+
+        self.play(Transform(circle, square))
+        self.wait(1)
+
+        self.play(FadeOut(circle), FadeOut(title))
